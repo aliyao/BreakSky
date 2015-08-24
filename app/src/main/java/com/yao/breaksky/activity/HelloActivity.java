8126c25137012a88d17cb864e3c5ceec20242221
nideyoyo <90508312@qq.com>
2015-08-25 05:03:33 +0800

    修改提示标题

--- app/src/main/java/com/yao/breaksky/activity/HelloActivity.java
+++ app/src/main/java/com/yao/breaksky/activity/HelloActivity.java
@@ -16,7 +16,7 @@ import android.view.View;
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  *
- * @see SystemUiHider
+ * @see  SystemUiHider
  */
 public class HelloActivity extends Activity {
     /**
