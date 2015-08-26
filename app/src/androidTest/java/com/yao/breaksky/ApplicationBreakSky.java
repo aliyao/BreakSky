587b933212e3b53ccf9ec329af22aed337630d60
ly <ly@jianzhimao.com>
2015-08-26 20:08:29 +0800

    Initial commit

---
+++ app/src/androidTest/java/com/yao/breaksky/ApplicationBreakSky.java
@@ -0,0 +1,13 @@
+package com.yao.breaksky;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationBreakSky extends ApplicationTestCase<Application> {
+    public ApplicationBreakSky() {
+        super(Application.class);
+    }
+}
