2fa3e31234598fa4da7511716cbad9024e1d48da
ly <ly@jianzhimao.com>
2015-08-28 11:06:43 +0800

    接入KJFrameForAndroid

---
+++ app/src/main/java/com/yao/breaksky/bean/FindBean.java
@@ -0,0 +1,13 @@
+package com.yao.breaksky.bean;
+
+/**
+ * 项目名称：BreakSky
+ * 类描述：
+ * 创建人：yoyo
+ * 创建时间：2015/8/28 11:06
+ * 修改人：yoyo
+ * 修改时间：2015/8/28 11:06
+ * 修改备注：
+ */
+public class FindBean {
+}
